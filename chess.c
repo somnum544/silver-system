a74d8f5eba760cb274177d774ccec4d3ae68b41e
Baestriansky <lisalpatrik2@gmail.com>
2025-02-04 22:24:19 +0100

    fixed move checking, piece moving, and game is now playable. Game is played in reverse where white moves(E2 - E4) move black pieces cuz matrix

--- chess.c
+++ chess.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define GREEN "\e[0;32m"
 #define RED "\e[0;31m"
@@ -9,7 +10,7 @@
 
 char** create_board();
 char** place_pieces(char** board, const char black_pieces[], const char white_pieces[]);
-char** make_move(board);
+char** make_move(char **board, char *move);
 int check_valid_input(char *move);
 void print_board(char **board);
 
@@ -19,8 +20,8 @@ void print_board(char **board);
     /*********************/
 
 int main(void) {
-    const char black_pieces[] = {'♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'};
-    const char white_pieces[] = {'♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'};
+    const char black_pieces[] = {'r','n','b','q','k','r','n','b'}; //{'♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'};
+    const char white_pieces[] ={'R','N','B','Q','K','R','N','B'}; //{'♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'};
     
     char** board = create_board();  
     place_pieces(board, black_pieces, white_pieces);
@@ -30,9 +31,17 @@ int main(void) {
 
     // Main loop
     while (1) {
+        // Stores the move from the input
         char move[5];
         scanf("%s", move);
-
+        // Checking valid input
+        if (!check_valid_input(move)) {
+            printf("Invalid move format");
+            continue;
+        }
+        // Moving the piece
+        make_move(board, move);
+        print_board(board);
     }
 
     // Free memory after use
@@ -51,34 +60,24 @@ int main(void) {
 char** make_move(char** board, char *move) {
     const char squares[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
     
-    // Index of squared moved FROM
-    int index_curr = 0;
-    for (int i = 0; i < SIZE; i++) {
-        if(move[0] == squares[i]) {
-            index_curr = i;
-        }
-    }
+    int column_from = move[0] - 'A';
+    int row_from = move[1] - '1';
+    int column_to = move[2] - 'A';
+    int row_to = move[3] - '1';
 
-    // Index of squared moved TO
-    int index_next = 0;
-    for (int i = 0; i < SIZE; i++) {
-        if(move[2] == squares[i]) {
-            index_next = i;
-        }
-    }
+    board[row_to][column_to] = board[row_from][column_from];
+    board[row_from][column_from] = '_';
 
-    // Moving the piece
-    board[index_next][move[3] - 1] = board[index_curr][move[1] - 1];
-    
-    // Clearing square moved from
-    board[index_curr][move[1] - 1] = '_';
     return board;
 }
 
 int check_valid_input(char *move) {
     
-    if (('A' <= (move[0] && move[2] >= 'H')) && (1 <= (move[1] && move[3]) >= 8)) return 1;
-    return 0;
+    return strlen(move) == 4 &&
+           move[0] >= 'A' && move[0] <= 'H' &&
+           move[1] >= '1' && move[1] <= '8' &&
+           move[2] >= 'A' && move[2] <= 'H' &&
+           move[3] >= '1' && move[3] <= '8';
 }
 
 void print_board(char **board) {
@@ -96,13 +95,13 @@ char** place_pieces(char** board, const char black_pieces[], const char white_pi
     // Place BLACK pieces
     for(int i = 0; i < SIZE; i++) {
         board[0][i] = black_pieces[i];
-        board[1][i] = '♟';
+        board[1][i] = 'p';     //♟
     }
     
     // Place WHITE pieces
     for(int i = 0; i < SIZE; i++) {
         board[7][i] = white_pieces[i];
-        board[6][i] = '♙';
+        board[6][i] = 'P';       //♙
     }
 
     // printing BLANK chars
@@ -115,9 +114,9 @@ char** place_pieces(char** board, const char black_pieces[], const char white_pi
 }
 
 char** create_board() {
-    char** board = malloc(SIZE * sizeof(int*));  // Allocate space for the rows
+    char** board = malloc(SIZE * sizeof(char*));  // Allocate space for the rows
     for (int i = 0; i < SIZE; i++){
-        board[i] = malloc(SIZE * sizeof(int));  // Allocate space for columns
+        board[i] = malloc(SIZE * sizeof(char));  // Allocate space for columns
     }
     
     return board;
