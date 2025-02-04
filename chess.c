57b5dd54abccff05c00abf9db9dbdc4c9d3e4696
Baestriansky <lisalpatrik2@gmail.com>
2025-02-04 05:42:08 +0100

    na ty negre

--- chess.c
+++ chess.c
@@ -1,21 +1,87 @@
-// ahojky :PP
-// BALLS
-// twuj tata
 
 #include <stdio.h>
 #include <stdlib.h>
+
+
+#define GREEN "\e[0;32m"
+#define RED "\e[0;31m"
 #define SIZE 8
+
+
+char** create_board();
+char** place_pieces(char** board, const char black_pieces[], const char white_pieces[]);
+char** make_move(board);
+int check_valid_input(char *move);
+
+//TODO make move, check valid move, 
+
+
+
+int main(void){
+    
+    const char black_pieces[] = {'♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'};
+    const char white_pieces[] = {'♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'};
+    
+    char** board = create_board();  
+    place_pieces(board, black_pieces, white_pieces);
+    
+    // Print the board
+    print_board(board);
+
+    while (1){
+    
+        char move[5];
+        scanf("%s", move);
+        
+        //check_correct_input
+        //check_valid_move
+        //make_move(move);
+        //print_board(board);
+
+    }
+
+    // Free memory after use
+    for (int i = 0; i < SIZE; i++){
+        free(board[i]);
+    }
+    free(board);
+
+    return 0;
+}
+
+
+
+
+
+
+
+char** make_move(board){}
+int check_valid_input(char *move){
+    
+}
+
+void print_board(char **board){
+
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 char** place_pieces(char** board, const char black_pieces[], const char white_pieces[]){
+    
     // Place black pieces
     for(int i = 0; i < SIZE; i++){
         board[0][i] = black_pieces[i];
-        board[1][i] = 'p';
+        board[1][i] = '♟';
     }
     
     // Place white pieces
     for(int i = 0; i < SIZE; i++){
         board[7][i] = white_pieces[i];
-        board[6][i] = 'P';
+        board[6][i] = '♙';
     }
 
     for(int i = 2; i<6; i++){
@@ -33,29 +99,4 @@ char** create_board(){
     }
     
     return board;
-}
-
-int main(void){
-    
-    const char black_pieces[] = {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'};
-    const char white_pieces[] = {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
-    
-    char** board = create_board();  
-    place_pieces(board, black_pieces, white_pieces);
-    
-    // Print the board
-    for (int i = 0; i < SIZE; i++){
-        for (int j = 0; j < SIZE; j++){
-            printf("%c ", board[i][j]);
-        }
-        printf("\n");
-    }
-
-    // Free memory after use
-    for (int i = 0; i < SIZE; i++){
-        free(board[i]);
-    }
-    free(board);
-
-    return 0;
 }
